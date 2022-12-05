bb73f012ff6b9e8c7d5744c4eea402cc92925afd
IgorProchmann <102532200+IgorProchmann@users.noreply.github.com>
2022-12-05 11:59:59 -0300

    Atividade em Duplas sobre tabelas SQL

---
+++ Fundamentos de Banco de Dados.java
@@ -0,0 +1,24 @@
+<h1>Atividade em duplas<h1>
+
+<h2>Nesse atividade seria realizado em duplas, e teria que desenvolver um sistema completo de banco de dados para um <h2>
+<h2>controle de estoque e vendas de uma loja de sua preferência. Desenvolver o modelo entidade relacionamento e criar as tabelas em SQL.<h2>
+
+<h2>CREATE TABLE Limao(<h2>
+<h2>  gelo int NOT NULL,<h2>
+<h2>  Açucar int NOT NULL,<h2>
+<h2>  PRIMARY KEY(Açucar)<h2>
+<h2>)<h2>
+
+<h2>CREATE TABLE Funcionarios(<h2>
+<h2>  Nome VARCHAR(20) NOT NULL,<h2>
+<h2>  idade int NOT NULL,<h2>
+<h2>  ID int NOT NULL,<h2>
+<h2>  PRIMARY KEY(ID)<h2>
+<h2>)<h2>
+
+<h2>CREATE TABLE Cliente(<h2>
+<h2>  nome VARCHAR(20) NOT NULL,<h2>
+<h2>  gastos int NOT NULL,<h2>
+<h2>  pedido int NOT NULL,<h2>
+<h2>  PRIMARY KEY(nome)<h2>
+<h2>)<h2>
