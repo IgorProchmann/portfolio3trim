fd9a61521d3ad52370a71ef8b05e078b0f2edd36
IgorProchmann <102532200+IgorProchmann@users.noreply.github.com>
2022-12-05 11:49:20 -0300

    Atividade de Herança

---
+++ Lógica de Programação.java
@@ -0,0 +1,3 @@
+<h1> Atividade de Herança: <h1>
+<h2>Nessa atividade o aluno deveria postar o método "cadastraAluno", similar ao "cadastraPessoa" do arquivo em anexo em ORIENTAÇÃO A OBJETOS<h2>
+<h2>link do trabalho: https://classroom.google.com/u/1/c/NDY5MzM1NDQyNjM4/a/NDkwMTkzMDcwMzUw/details <h2>
